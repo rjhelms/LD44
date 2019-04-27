168bbd97ff19afeab73fe7a8295f3f092052f766
Rob Hailman <robhailman@gmail.com>
2019-04-27 17:27:31 -0400

    Add edge scrolling

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 targetPosition = new Vector3(0,0,-10);
+    [SerializeField]
+    private Vector2 targetShift = new Vector2(8, 6);
+    [SerializeField]
+    private float targetLerpSpeed = 0.9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, targetLerpSpeed * Time.deltaTime);
+    }
+
+    public void ShiftCamera(Vector2 shift)
+    {
+        targetPosition += (Vector3)(shift * targetShift);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : BaseActor
 {
-   
+
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -26,5 +23,26 @@ public class PlayerController : BaseActor
         float y_move = Input.GetAxisRaw("Vertical");
         moveVector = new Vector2(x_move, y_move);
         moveVector.Normalize();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "ScreenBounds")
+        {
+            Vector2 shift = new Vector2(0, 0);
+            if (collision.gameObject.name == "LeftScreenEdge")
+            { shift.x = -1; }
+            else if (collision.gameObject.name == "RightScreenEdge")
+            { shift.x = 1; }
+            else if (collision.gameObject.name == "TopScreenEdge")
+            {
+                shift.y = 1;
+            }
+            else if (collision.gameObject.name == "BottomScreenEdge")
+            {
+                shift.y = -1;
+            }
+            FindObjectOfType<CameraController>().ShiftCamera(shift);
+        }
     }
 }
