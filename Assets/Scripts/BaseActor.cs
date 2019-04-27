020f078354302e231036df40c66153dfc1a41745
Rob Hailman <robhailman@gmail.com>
2019-04-27 16:50:22 -0400

    Add player movement
    Added BaseActor class to hold things common to all actor entities

---
+++ Assets/Scripts/BaseActor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
+    INVALID
+}
+
+[Serializable]
+public class WalkCycle
+{
+    public Sprite[] walkSprites;
+}
+
+public class BaseActor : MonoBehaviour
+{
+    [SerializeField]
+    protected Sprite[] idleSprites;
+    [SerializeField]
+    protected WalkCycle[] walkCycles;
+    [SerializeField]
+    protected float animTime = .2f;
+    [SerializeField]
+    protected Direction direction;
+    [SerializeField]
+    protected float moveSpeed;
+    [SerializeField]
+    protected Vector2 moveVector = new Vector2(0f, 0f);
+
+    float nextSpriteChangeTime;
+    SpriteRenderer spriteRenderer;
+    int idxSprite = 0;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer.sprite = walkCycles[(int)direction].walkSprites[idxSprite];
+
+        nextSpriteChangeTime = Time.time + animTime;
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (Time.time >= nextSpriteChangeTime)
+        {
+            idxSprite++;
+            idxSprite %= walkCycles[(int)direction].walkSprites.Length;
+            nextSpriteChangeTime += animTime;
+        }
+        UpdateDirection();
+        UpdateSprite();
+        rb.velocity = moveVector * moveSpeed;
+    }
+
+    void UpdateDirection()
+    {
+        if (moveVector.magnitude == 0)
+            return;
+        float move_angle = Vector2.SignedAngle(Vector2.right, moveVector);
+        Debug.Log(move_angle);
+        if (-180 <= move_angle && move_angle < -135)
+        {
+            direction = Direction.WEST;
+        }
+        else if (-135 <= move_angle && move_angle <= -45)
+        {
+            direction = Direction.SOUTH;
+        }
+        else if (-45 < move_angle && move_angle <= 45)
+        {
+            direction = Direction.EAST;
+        }
+        else if (45 < move_angle && move_angle < 135)
+        {
+            direction = Direction.NORTH;
+        }
+        else if (135 <= move_angle && move_angle <= 180)
+        {
+            direction = Direction.WEST;
+        }
+    }
+
+    void UpdateSprite()
+    {
+        if (moveVector.magnitude == 0)
+        {
+            spriteRenderer.sprite = idleSprites[(int)direction];
+            idxSprite = 0;
+        }
+        else
+        {
+            spriteRenderer.sprite = walkCycles[(int)direction].walkSprites[idxSprite];
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,71 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Direction
+public class PlayerController : BaseActor
 {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST,
-    INVALID
-}
-
-[Serializable]
-public class WalkCycle
-{
-    public Sprite[] walkSprites;
-}
-
-public class PlayerController : MonoBehaviour
-{
-    [SerializeField]
-    Sprite[] idleSprites;
-    [SerializeField]
-    WalkCycle[] walkCycles;
-    [SerializeField]
-    float animTime = .2f;
-    [SerializeField]
-    float rotateTime = 2f;
-    [SerializeField]
-    Direction direction;
-
-    float nextSpriteChangeTime;
-    float nextRotateTime;
-
-    int idxSprite = 0;
-
-    SpriteRenderer spriteRenderer;
+   
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        nextSpriteChangeTime = Time.time + animTime;
-        nextRotateTime = Time.time + rotateTime;
-        spriteRenderer.sprite = walkCycles[(int)direction].walkSprites[idxSprite];
+        base.Start();
+
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        if (Time.time >= nextRotateTime)
-        {
-            direction++;
-            if (direction == Direction.INVALID)
-            {
-                direction = Direction.NORTH;
-            }
-            idxSprite = 0;
-            spriteRenderer.sprite = walkCycles[(int)direction].walkSprites[idxSprite];
-            nextRotateTime += rotateTime;
-            nextSpriteChangeTime = Time.time + animTime;
-        }
-        
-        if (Time.time >= nextSpriteChangeTime)
-        {
-            idxSprite++;
-            idxSprite %= walkCycles[(int)direction].walkSprites.Length;
-            spriteRenderer.sprite = walkCycles[(int)direction].walkSprites[idxSprite];
-            nextSpriteChangeTime += animTime;
-        }
+        ProcessInput();
+        base.Update();
+    }
+
+    void ProcessInput()
+    {
+        float x_move = Input.GetAxisRaw("Horizontal");
+        float y_move = Input.GetAxisRaw("Vertical");
+        moveVector = new Vector2(x_move, y_move);
+        moveVector.Normalize();
     }
 }
