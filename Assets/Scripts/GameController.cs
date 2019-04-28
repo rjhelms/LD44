f9b801abf2bdf9e882b84439d97a94c1e3c1c812
Rob Hailman <robhailman@gmail.com>
2019-04-28 11:43:17 -0400

    Cereal stuff and towards a UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    private Camera mainCamera;
+
+    [SerializeField]
+    private Canvas canvasUI;
 
     [SerializeField]
     private int CerealRemaining;
@@ -11,13 +17,19 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = FindObjectOfType<Camera>();
+        canvasUI = GameObject.Find("UICanvas").GetComponent<Canvas>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateUI()
+    {
+        canvasUI.GetComponent<CanvasScaler>().scaleFactor = mainCamera.GetComponent<PixelPerfectCamera>().pixelRatio;
     }
 
     public void RegisterCereal(GameObject cereal)
