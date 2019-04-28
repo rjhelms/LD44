4c21d25374aa07e7798b276952c0d3781101411d
Rob Hailman <robhailman@gmail.com>
2019-04-28 10:24:03 -0400

    Add projectile timeout

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,17 +15,27 @@ public class Projectile : MonoBehaviour
 
     [SerializeField]
     string targetTag;
+
+    [SerializeField]
+    float lifespanTime;
+
     private Rigidbody2D rb;
+    private float lifespanEnd;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        lifespanEnd = Time.time + lifespanTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time > lifespanEnd)
+        {
+            Destroy(gameObject);
+        }
         rb.velocity = moveVector * moveSpeed;
         transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
     }
