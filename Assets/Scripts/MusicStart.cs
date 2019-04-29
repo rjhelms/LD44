836f56091583948ea912010eee55957c58500eb0
Rob Hailman <robhailman@gmail.com>
2019-04-28 20:53:38 -0400

    Add music

---
+++ Assets/Scripts/MusicStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicStart : MonoBehaviour
+{
+    public GameObject MusicPrefab;
+
+    private GameObject musicPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicPlayer = GameObject.Find("MusicPlayer");
+        if (musicPlayer == null)
+        {
+            musicPlayer = Instantiate(MusicPrefab);
+            DontDestroyOnLoad(musicPlayer);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
